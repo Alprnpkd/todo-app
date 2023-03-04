6d5fdd68c074f1037668987f58fbc91043989bf9
Alp <yazilimAlp@gmail.com>
2023-03-04 22:56:11 +0300

    github pages için derlenmış dosyalar yüklendi

--- src/app.js
+++ src/app.js
@@ -99,7 +99,7 @@ const path = myIcon.querySelector("path");
 }
     
 else {
-    changeMode.src="/images/icon-sun.svg";
+    changeMode.src="icon-sun.svg";
     bgImage.className="bg-img-dark" ;
     body.style.backgroundColor="hsl(235, 21%, 11%)" ;
     changeMode.className="dark" ;
